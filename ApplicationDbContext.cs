29f1a9ee09d9bce4951bbfe045ce065b031dff71
Ali SABOURI <72344723+alisabourii@users.noreply.github.com>
2024-05-18 00:20:08 +0300

    Add files via upload

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Auctions.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Auctions.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Listing> Listing { get; set; }
+        public DbSet<Bid> Bid { get; set; }
+        public DbSet<Comment> Comment { get; set; }
+    }
+}
