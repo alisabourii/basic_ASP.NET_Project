d0f0e5929672b16d2219e1c3ec04cccf7e7636c4
Ali SABOURI <72344723+alisabourii@users.noreply.github.com>
2024-05-18 00:19:56 +0300

    Add files via upload

---
+++ ListingService.cs
@@ -0,0 +1,27 @@
+﻿using Auctions.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Auctions.Data.Services
+{
+    public class ListingService : IlListingService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ListingService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Add(Listing listing)
+        {
+            _context.Listing.Add(listing);
+            await _context.SaveChangesAsync();
+        }
+
+        public IQueryable<Listing> GetAll()
+        {
+            var applicationDbContext = _context.Listing.Include(l => l.User);
+            return applicationDbContext;
+        }
+    }
+}
